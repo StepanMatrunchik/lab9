88c9f8e53202fbd73c10e96b6e3e3c3adb495383
StepanMatrunchik <84409912+StepanMatrunchik@users.noreply.github.com>
2022-11-20 20:54:59 +0300

    minor code fixes

--- src/test/java/MyGloTest.java
+++ src/test/java/MyGloTest.java
@@ -23,19 +23,18 @@ public class MyGloTest {
     @Test
     public void increaseAmountOfItemByPlusButton(){
         driver.get("https://shop.myglo.by/catalog/glo-hyper-x2-belo-zolotoy/");
-        //div[@class='btn btn--primary']
+        
         new WebDriverWait(driver, Duration.ofSeconds(10))
                 .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class='btn btn--primary']")));
         WebElement ageConfirmation = driver.findElement(By.xpath("//div[@class='btn btn--primary']"));
         ageConfirmation.click();
-        //input[@class='counter-input js-counter-input']
+        
         new WebDriverWait(driver, Duration.ofSeconds(10))
                 .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@class='counter-input js-counter-input']")));
         WebElement amountInput = driver.findElement(By.xpath("//input[@class='counter-input js-counter-input']"));
         amountInput.sendKeys(Keys.BACK_SPACE);
         amountInput.sendKeys("4");
-
-        //button[@class='counter-btn counter-plus js-counter-plus']
+        
         WebElement plusButton = driver.findElement(By.xpath("//button[@class='counter-btn counter-plus js-counter-plus']"));
         plusButton.click();
         Assert.assertEquals(amountInput.getAttribute("value"),"5","amount is not 5, its "+amountInput.getAttribute("value"));
